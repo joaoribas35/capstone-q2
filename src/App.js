e8a866cdaf9d9315b5db6a0de79eac699a8c2579
Mateus Maia <eng.fonsecamateus@gmail.com>
2021-04-08 19:52:27 +0000

    Revert "Merge branch 'mssBlueContainer' into 'develop'"
    This reverts merge request !2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
+
 import GlobalStyle from "./global/styles";
-import {Container} from './styles.js'
 
 function App() {
   return (
     <>
-      <div>Meu app</div>
-      <GlobalStyle />
-      <Container>
-        <h1>ola</h1>
-        <h2>como vai</h2>
-      </Container>
+      <div>Meu app</div>;
+      <GlobalStyle />;
     </>
   );
 }
