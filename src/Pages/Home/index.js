9d5d441d7d9e9f908d109eef07756202ead238cb
JuarezGoesJunior <juarezantoniog12@gmail.com>
2021-04-11 19:09:06 -0300

    refactory home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,5 +1,3 @@
-import DoughnutChart from "../../components/DoughnutChart";
-import LineChart from "../../components/LineChart";
 import MenuNavBar from "../../components/MenuNavBar";
 
 const Home = () => {
@@ -7,8 +5,6 @@ const Home = () => {
     <div style={{ textAlign: "center" }}>
       <MenuNavBar />
       <h1>Bem Vindo a minha Aplicação</h1>
-      <DoughnutChart />
-      <LineChart />
     </div>
   );
 };
