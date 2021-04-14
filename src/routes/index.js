d350686797145ad2b45a2e009d54519d6c3a2490
Diego Silva <dev.diegosilva@hotmail.com>
2021-04-14 17:11:50 -0300

    feat: Adicionei a Landing page nas rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,11 +8,13 @@ import Dashboard from "../Pages/Dashboard/";
 import Transactions from "../Pages/Transactions";
 
 import Home from "../Pages/Home";
+import HomePage from "../Pages/HomePage";
 
 const Routes = () => {
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/home" component={Home} />
       <Route exact path="/login" component={LoginPage} />
       <Route exact path="/register" component={RegisterPage} />
       <Route exact path="/rebalance" component={Rebalance} />
