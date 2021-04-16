a110b953f4f974e24912449bdbb6e95484f6dd5e
Diego Silva <dev.diegosilva@hotmail.com>
2021-04-16 15:26:00 -0300

    update

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -10,24 +10,6 @@ import * as S from "./style";
 const HomePage = () => {
   return (
     <>
-<<<<<<< HEAD
-      <S.LandingPage>
-        <TopBarHomePage />
-        <S.Sumary>
-          <S.AppSumary>
-            <h1>Cripto Facil</h1>
-            <p>
-              É um app gerenciador de carteiras de criptos, Aqui no Nosso app
-              voce consegue organizar suas carteirase e seu historico de
-              transação. Tem tambem a parte de Carteira Ideal que voce pode
-              colocar qual a porcentagem de cada moeda voce quer.{" "}
-              <Link to="/register">comece agora</Link>
-            </p>
-          </S.AppSumary>
-          <SimpleSlider />
-        </S.Sumary>
-      </S.LandingPage>
-=======
       <ParticlesBackground>
         <S.LandingPage>
           <TopBarHomePage />
@@ -48,7 +30,6 @@ const HomePage = () => {
           </S.Sumary>
         </S.LandingPage>
       </ParticlesBackground>
->>>>>>> 1306da3cafe70c164e019a056bf7bff8d6cd5516
     </>
   );
 };
